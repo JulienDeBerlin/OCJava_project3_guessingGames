8e9388e42d50322b76b0b1948fce1cec539f02cc
JulienDeBerlin <julien_berthoud@yahoo.fr>
2018-06-15 17:32:12 +0200

    Jeu1 ok, Jeu2 Mode 1 et 2 ok

--- src/com/berthoud/Main.java
+++ src/com/berthoud/Main.java
@@ -5,25 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Jeu2 partie1 = new Jeu2ModeDefense(4, 15, 10);
-
-        ((Jeu2ModeDefense) partie1).reponseValidationJoueur();
-
-        partie1.play();
-
-
-//        for(int i=0; i<args.length; i++) {
-//            System.out.println("Parameter "+i+" is: "+ args[i]);
-//        }
-
-//        Jeu1 partie1 = new Jeu1ModeAttaque(4, 4);
+//        Jeu2 partie1 = new Jeu2ModeDefense(4, 15, 6);
+//
 //        partie1.play();
-//
-//        Jeu1ModeDefense partie2 = new Jeu1ModeDefense(4, 6);
-//        partie2.play();
-//
-//        Jeu1ModeDuel partie3 = new Jeu1ModeDuel(2, 6);
-//        partie3.play();
+
+        Jeu2 partie2 = new Jeu2ModeAttaque(3, 15, 3);
+
+        partie2.play();
+
+
+
 
 
 
